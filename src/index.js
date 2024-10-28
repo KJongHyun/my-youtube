cab2b64e027e0d01a7210d7f12083ea508055b0e
luffy <whdgus8219@naver.com>
2024-10-29 00:17:00 +0900

    feat: 기본 프레임 작업

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+import NotFound from './pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        path: '/videos',
+        element: <Videos />,
+      },
+      {
+        path: '/videos/:keyword',
+        element: <Videos />,
+      },
+      {
+        path: '/videos/watch/:id',
+        element: <VideoDetail />,
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
