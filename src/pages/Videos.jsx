655408e751d87542ab7f2fed0d2d0ca9c7724484
luffy <whdgus8219@naver.com>
2024-10-30 22:22:56 +0900

    feat: mock data 버전 추가

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from './VideoCard';
+import { search } from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutube';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: () => {
+      const youtube = new FakeYoutube();
+      return youtube.search(keyword);
+    },
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
